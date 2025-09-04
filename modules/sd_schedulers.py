058f1f0842f995cd7d16d0afc5d95c558c65147f
Haoming <hmstudy02@gmail.com>
2025-09-04 22:57:33 +0800

    schedulers

--- modules/sd_schedulers.py
+++ modules/sd_schedulers.py
@@ -1,16 +1,16 @@
 import dataclasses
 from math import atan, pi
+from typing import Callable
 
 import k_diffusion
 import numpy as np
 import torch
+from modules import shared
 from scipy import stats
 
-from modules import shared
 
-
-def to_d(x, sigma, denoised):
-    """Converts a denoiser output to a Karras ODE derivative."""
+def to_d(x: torch.Tensor, sigma: float, denoised: torch.Tensor):
+    """Converts a denoiser output to a Karras ODE derivative"""
     return (x - denoised) / sigma
 
 
@@ -21,69 +21,11 @@ k_diffusion.sampling.to_d = to_d
 class Scheduler:
     name: str
     label: str
-    function: any
+    function: Callable
 
-    default_rho: float = -1
+    default_rho: float = -1.0
     need_inner_model: bool = False
-    aliases: list = None
-
-
-def uniform(n, sigma_min, sigma_max, inner_model, device):
-    return inner_model.get_sigmas(n).to(device)
-
-
-def sgm_uniform(n, sigma_min, sigma_max, inner_model, device):
-    start = inner_model.sigma_to_t(torch.tensor(sigma_max))
-    end = inner_model.sigma_to_t(torch.tensor(sigma_min))
-    sigs = [inner_model.t_to_sigma(ts) for ts in torch.linspace(start, end, n + 1)[:-1]]
-    sigs += [0.0]
-    return torch.FloatTensor(sigs).to(device)
-
-
-def get_align_your_steps_sigmas(n, sigma_min, sigma_max, device):
-    # https://research.nvidia.com/labs/toronto-ai/AlignYourSteps/howto.html
-    def loglinear_interp(t_steps, num_steps):
-        """
-        Performs log-linear interpolation of a given array of decreasing numbers.
-        """
-        xs = np.linspace(0, 1, len(t_steps))
-        ys = np.log(t_steps[::-1])
-
-        new_xs = np.linspace(0, 1, num_steps)
-        new_ys = np.interp(new_xs, xs, ys)
-
-        interped_ys = np.exp(new_ys)[::-1].copy()
-        return interped_ys
-
-    if shared.sd_model.is_sdxl:
-        sigmas = [14.615, 6.315, 3.771, 2.181, 1.342, 0.862, 0.555, 0.380, 0.234, 0.113, 0.029]
-    else:
-        # Default to SD 1.5 sigmas.
-        sigmas = [14.615, 6.475, 3.861, 2.697, 1.886, 1.396, 0.963, 0.652, 0.399, 0.152, 0.029]
-
-    if n != len(sigmas):
-        sigmas = np.append(loglinear_interp(sigmas, n), [0.0])
-    else:
-        sigmas.append(0.0)
-
-    return torch.FloatTensor(sigmas).to(device)
-
-
-def kl_optimal(n, sigma_min, sigma_max, device):
-    alpha_min = torch.arctan(torch.tensor(sigma_min, device=device))
-    alpha_max = torch.arctan(torch.tensor(sigma_max, device=device))
-    step_indices = torch.arange(n + 1, device=device)
-    sigmas = torch.tan(step_indices / n * alpha_min + (1.0 - step_indices / n) * alpha_max)
-    return sigmas
-
-
-def simple_scheduler(n, sigma_min, sigma_max, inner_model, device):
-    sigs = []
-    ss = len(inner_model.sigmas) / n
-    for x in range(n):
-        sigs += [float(inner_model.sigmas[-(1 + int(x * ss))])]
-    sigs += [0.0]
-    return torch.FloatTensor(sigs).to(device)
+    aliases: list[str] = None
 
 
 def normal_scheduler(n, sigma_min, sigma_max, inner_model, device, sgm=False, floor=False):
@@ -103,6 +45,64 @@ def normal_scheduler(n, sigma_min, sigma_max, inner_model, device, sgm=False, fl
     return torch.FloatTensor(sigs).to(device)
 
 
+def simple_scheduler(n, sigma_min, sigma_max, inner_model, device):
+    sigs = []
+    ss = len(inner_model.sigmas) / n
+    for x in range(n):
+        sigs += [float(inner_model.sigmas[-(1 + int(x * ss))])]
+    sigs += [0.0]
+    return torch.FloatTensor(sigs).to(device)
+
+
+def uniform(n, sigma_min, sigma_max, inner_model, device):
+    return inner_model.get_sigmas(n).to(device)
+
+
+def sgm_uniform(n, sigma_min, sigma_max, inner_model, device):
+    start = inner_model.sigma_to_t(torch.tensor(sigma_max))
+    end = inner_model.sigma_to_t(torch.tensor(sigma_min))
+    sigs = [inner_model.t_to_sigma(ts) for ts in torch.linspace(start, end, n + 1)[:-1]]
+    sigs += [0.0]
+    return torch.FloatTensor(sigs).to(device)
+
+
+def _loglinear_interp(t_steps, num_steps):
+    """Performs log-linear interpolation of a given array of decreasing numbers"""
+    xs = np.linspace(0, 1, len(t_steps))
+    ys = np.log(t_steps[::-1])
+
+    new_xs = np.linspace(0, 1, num_steps)
+    new_ys = np.interp(new_xs, xs, ys)
+
+    interped_ys = np.exp(new_ys)[::-1].copy()
+    return interped_ys
+
+
+def get_align_your_steps_sigmas(n, sigma_min, sigma_max, device):
+    """https://research.nvidia.com/labs/toronto-ai/AlignYourSteps/howto.html"""
+
+    if shared.sd_model.is_sdxl:
+        sigmas = sigmas = [sigma_max, sigma_max / 2.314, sigma_max / 3.875, sigma_max / 6.701, sigma_max / 10.89, sigma_max / 16.954, sigma_max / 26.333, sigma_max / 38.46, sigma_max / 62.457, sigma_max / 129.336, 0.029]
+    else:
+        # Default to SD 1.5 sigmas.
+        sigmas = [sigma_max, sigma_max / 2.257, sigma_max / 3.785, sigma_max / 5.418, sigma_max / 7.749, sigma_max / 10.469, sigma_max / 15.176, sigma_max / 22.415, sigma_max / 36.629, sigma_max / 96.151, 0.029]
+
+    if n != len(sigmas):
+        sigmas = np.append(_loglinear_interp(sigmas, n), [0.0])
+    else:
+        sigmas.append(0.0)
+
+    return torch.FloatTensor(sigmas).to(device)
+
+
+def kl_optimal(n, sigma_min, sigma_max, device):
+    alpha_min = torch.arctan(torch.tensor(sigma_min, device=device))
+    alpha_max = torch.arctan(torch.tensor(sigma_max, device=device))
+    step_indices = torch.arange(n + 1, device=device)
+    sigmas = torch.tan(step_indices / n * alpha_min + (1.0 - step_indices / n) * alpha_max)
+    return sigmas
+
+
 def ddim_scheduler(n, sigma_min, sigma_max, inner_model, device):
     sigs = []
     ss = max(len(inner_model.sigmas) // n, 1)
@@ -116,14 +116,25 @@ def ddim_scheduler(n, sigma_min, sigma_max, inner_model, device):
 
 
 def beta_scheduler(n, sigma_min, sigma_max, inner_model, device):
-    # From "Beta Sampling is All You Need" [arXiv:2407.12173] (Lee et. al, 2024) """
+    """
+    Beta scheduler
+    Based on "Beta Sampling is All You Need" [arXiv:2407.12173] (Lee et. al, 2024)
+    """
     alpha = shared.opts.beta_dist_alpha
     beta = shared.opts.beta_dist_beta
-    timesteps = 1 - np.linspace(0, 1, n)
-    timesteps = [stats.beta.ppf(x, alpha, beta) for x in timesteps]
-    sigmas = [sigma_min + (x * (sigma_max - sigma_min)) for x in timesteps]
-    sigmas += [0.0]
-    return torch.FloatTensor(sigmas).to(device)
+
+    total_timesteps = len(inner_model.sigmas) - 1
+    ts = 1 - np.linspace(0, 1, n, endpoint=False)
+    ts = np.rint(stats.beta.ppf(ts, alpha, beta) * total_timesteps)
+
+    sigs = []
+    last_t = -1
+    for t in ts:
+        if t != last_t:
+            sigs += [float(inner_model.sigmas[int(t)])]
+        last_t = t
+    sigs += [0.0]
+    return torch.FloatTensor(sigs).to(device)
 
 
 def turbo_scheduler(n, sigma_min, sigma_max, inner_model, device):
@@ -174,16 +185,16 @@ def bong_tangent_scheduler(n, sigma_min, sigma_max, device, *, start=1.0, middle
 
 schedulers = [
     Scheduler("automatic", "Automatic", None),
-    Scheduler("uniform", "Uniform", uniform, need_inner_model=True),
     Scheduler("karras", "Karras", k_diffusion.sampling.get_sigmas_karras, default_rho=7.0),
     Scheduler("exponential", "Exponential", k_diffusion.sampling.get_sigmas_exponential),
     Scheduler("polyexponential", "Polyexponential", k_diffusion.sampling.get_sigmas_polyexponential, default_rho=1.0),
+    Scheduler("normal", "Normal", normal_scheduler, need_inner_model=True),
+    Scheduler("simple", "Simple", simple_scheduler, need_inner_model=True),
+    Scheduler("uniform", "Uniform", uniform, need_inner_model=True),
     Scheduler("sgm_uniform", "SGM Uniform", sgm_uniform, need_inner_model=True, aliases=["SGMUniform"]),
     Scheduler("kl_optimal", "KL Optimal", kl_optimal),
-    Scheduler("align_your_steps", "Align Your Steps", get_align_your_steps_sigmas),
-    Scheduler("simple", "Simple", simple_scheduler, need_inner_model=True),
-    Scheduler("normal", "Normal", normal_scheduler, need_inner_model=True),
     Scheduler("ddim", "DDIM", ddim_scheduler, need_inner_model=True),
+    Scheduler("align_your_steps", "Align Your Steps", get_align_your_steps_sigmas),
     Scheduler("beta", "Beta", beta_scheduler, need_inner_model=True),
     Scheduler("turbo", "Turbo", turbo_scheduler, need_inner_model=True),
     Scheduler("bong_tangent", "Bong Tangent", bong_tangent_scheduler),
